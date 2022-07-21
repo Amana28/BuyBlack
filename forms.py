eb83bb7a903a82a90357dba20fc4596f18944702
Amana28 <76058143+Amana28@users.noreply.github.com>
2022-07-21 14:05:49 -0400

    removed TextField import

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm 
 from wtforms import validators, ValidationError  
-from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField  
+from wtforms import IntegerField, TextAreaField, SubmitField, RadioField, SelectField  
   
 class GetChoice(FlaskForm):     
    option = SelectField('Black Owned', choices = [('Restaurants', 'Restaurants'),('Barber Shops', 'Barbershops'),('Clothing Stores','Clothing Stores'),('Hair Salons', 'Hair Salons'),('Banks', 'Banks'), ('Coffee Shops', 'Coffee Shops'), ('Healthcare','Healthcare')])  
