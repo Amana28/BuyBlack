6533d90bc532642f1c2cf8d66d5f146585c81a70
Amana28 <76058143+Amana28@users.noreply.github.com>
2022-07-22 09:21:44 -0400

    api key hidden

--- buyblack.py
+++ buyblack.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, abort, request, flash
 from forms import GetChoice
+from dotenv import load_dotenv
 import requests
+import os
+
+
 app = Flask(__name__)
 app.secret_key = 'development key'
-API_KEY = "AIzaSyBIJAAFKaletYozLcOg413VGAdHqNbJzWY"
 LAT = 0
 LNG = 0
+
+def configure():
+    load_dotenv()
+
 @app.route("/")
 def index():
     return render_template('home.html') 
@@ -18,7 +25,7 @@ def get_coords():
     full_address = request.form["name"]
     print(type(full_address))
     params = {
-        'key': API_KEY,
+        'key': os.getenv('API_KEY'),
         'address': full_address
     }
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
@@ -32,7 +39,7 @@ def get_coords():
         form = GetChoice()
     else:
         return "address is invalid"
-    return render_template('search.html', form = form, latitude = LAT, longitude = LNG, url = "https://maps.googleapis.com/maps/api/js?key=" + API_KEY + "&callback=initMap") 
+    return render_template('search.html', form = form, latitude = LAT, longitude = LNG, url = "https://maps.googleapis.com/maps/api/js?key=" + os.getenv('API_KEY') + "&callback=initMap") 
 
 @app.route("/results", methods=['POST', 'GET'])
 def results():
@@ -47,7 +54,8 @@ def results():
     response = requests.get(base_url, params=params)
     data = response.json()
     businesses = data['results']
-    return render_template('results.html', form = form, choice = choice, businesses = businesses, latitude = LAT, longitude = LNG, url = "https://maps.googleapis.com/maps/api/js?key=" + API_KEY + "&callback=initMap") 
+    return render_template('results.html', form = form, choice = choice, businesses = businesses, latitude = LAT, longitude = LNG, url = "https://maps.googleapis.com/maps/api/js?key=" + os.getenv('API_KEY') + "&callback=initMap") 
         
 if __name__ == '__main__':
+    configure()
     app.run(debug=True, host="0.0.0.0")
